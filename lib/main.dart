3cd0daa391a29e560d3acba2f3a951e4bed1a90f
Soloman -Job <strelitcya@gmail.com>
2026-03-19 11:36:18 +0530

    First Prototype Build

--- lib/main.dart
+++ lib/main.dart
@@ -17,16 +17,24 @@
   class _FlagQuizState extends State<FlagQuiz> {
     int score = 0;
     int questionNumber = 1;
-    var countryList = ['france','japan','thailand','solomon'];
-    var answer = "";
-    List <Map<String,String>> questions =[];
+    List countryList = ['france','japan','thailand','solomon','india'];
+    var correctAnswer = "";
     @override
     void initState(){
     super.initState();
     countryList.shuffle();
-}
+    }
+   
+
+    
     @override
     Widget build(BuildContext context) {
+      correctAnswer = countryList[questionNumber-1];
+      List shuffleList = [];
+      List distractors = countryList.sublist(0,4).where((country) => country !=correctAnswer).toList();
+                shuffleList.add(correctAnswer);
+                shuffleList.addAll(distractors.take(3));
+                shuffleList.shuffle();
       return Scaffold(
           appBar: AppBar(
             title: Text("Flag Quiz"),
@@ -35,26 +43,42 @@
           body: Center(
             child : Column(
               children:[
+                
+    
                 Text("Question : $questionNumber"),
                 const SizedBox(height:20),
                 Text("What Flag is this?"),
                 Image.asset("assets/${countryList[questionNumber-1]}.png"),
-                for(var i in countryList)
-                buildOption(i, countryList[questionNumber-1]),
+                
+                for (var i in shuffleList)
+                buildOption(i, correctAnswer),
+                
                 SizedBox(height: 20),
                 
    
                 Text("Current Score : $score"),
-                reset()
+                Row(
+                  mainAxisAlignment:MainAxisAlignment.center,
+                  children: [
+                    
+                    reset(),
+                    finish()
+                  ]
+                    
+                )
+                
               ]
             ),
           ),
         );
     }
     Widget buildOption(String countryName,String answer){
-    return SizedBox(
+    return Padding(
+    padding:EdgeInsets.all(16),
+    child: SizedBox(
     width: 200,
-    height: 50,
+    height: 30,
+
     child: ElevatedButton(
     onPressed: () {
       if(countryName == answer){
@@ -76,14 +100,13 @@
       },
       child: Text(countryName),
     )
-    );
+    ));
   }
   Widget reset(){
-    return SizedBox(
-      width: 200,
-      height:50,
+    return Padding(
+      padding:EdgeInsets.all(2),
       child: ElevatedButton(
-        style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.red , foregroundColor: Colors.white),
         onPressed: (){
           setState(() {
             questionNumber=1;
@@ -95,6 +118,23 @@
       ),
     );
   
+  }
+  Widget finish(){
+    return Padding(padding: EdgeInsets.all(2),
+    child:ElevatedButton(
+      style:ElevatedButton.styleFrom(backgroundColor: Colors.green , foregroundColor: Colors.white),
+      onPressed: (){
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("score : $score"))
+        );
+        setState(() {
+          score = 0;
+        });
+        
+      },
+      child: Text("Finished"),
+    )
+    );
   }
   }
   
